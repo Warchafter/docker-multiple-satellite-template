e5630a71088e571ad66bf3efa271ea52c96a09e2
Kevin Arriaga <kevin@goldtone.com>
2025-08-13 16:58:05 -0400

    Modified variables for URL paths

--- services/backend-master/wp-config.php
+++ services/backend-master/wp-config.php
@@ -7,9 +7,7 @@ $db_port = getenv('WORDPRESS_DB_PORT');
 define('DB_HOST', $db_port ? $db_host . ':' . $db_port : $db_host);
 define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
 define('DB_USER', getenv('WORDPRESS_DB_USER'));
-define('DB_PASSWORD', file_exists(getenv('WORDPRESS_DB_PASSWORD_FILE'))
-    ? trim(file_get_contents(getenv('WORDPRESS_DB_PASSWORD_FILE')))
-    : getenv('WORDPRESS_DB_PASSWORD'));
+define('DB_PASSWORD', trim(file_get_contents(getenv('WORDPRESS_DB_PASSWORD_FILE'))));
 
 // Keys & Salts (use simple fallbacks for now)
 define('AUTH_KEY',         getenv('WP_AUTH_KEY') ?: 'put-your-unique-phrase-here-auth-key');
@@ -18,8 +16,8 @@ define('LOGGED_IN_KEY',    getenv('WP_LOGGED_IN_KEY') ?: 'put-your-unique-phrase
 define('NONCE_KEY',        getenv('WP_NONCE_KEY') ?: 'put-your-unique-phrase-here-nonce-key');
 
 $table_prefix = 'wp_';
-define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST']);
-define('WP_HOME',    'http://' . $_SERVER['HTTP_HOST']);
+define('WP_SITEURL', getenv('PUBLIC_WP_API'));
+define('WP_HOME',    getenv('PUBLIC_WP_API'));
 
 // Disable file edits, enforce REST usage
 define('DISALLOW_FILE_EDIT', true);
